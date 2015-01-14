d3d62bea3f73e7939f048a046a8c70da07b2927e
unknown <vcosta@damasco.tecgraf.puc-rio.br>
2015-01-14 15:04:20 -0200

    estou testando branches

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour {
     public Maze mazePrefab;
 
     private Maze mazeInstance;
-
+    //teste de branch
     private void BeginGame()
     {
         mazeInstance = Instantiate(mazePrefab) as Maze;
